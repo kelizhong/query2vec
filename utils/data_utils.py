ebc202a26766408cbb05daa094d01c6f7c9b9f20
Zhong, keli <qzk@126.com>
2017-02-13 16:02:26 +0800

    new seq2seq

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -227,3 +227,33 @@ def read_data(source_path, max_size=None):
           break
       source, target = source_file.readline(), source_file.readline()
   return data_set
+
+def read_data_without_bucket(source_path, max_size=None):
+  """Read data from source and target files and put into buckets.
+     Data is considered one line after the other. (input -> output)
+  Args:
+    source_path: path to the files with token-ids for the source language.
+    max_size: maximum number of lines to read, all other will be ignored;
+      if 0 or None, data files will be read completely (no limit).
+
+  Returns:
+    data_set: a list of length len(_buckets); data_set[n] contains a list of
+      (source, target) pairs read from the provided data files that fit
+      into the n-th bucket, i.e., such that len(source) < _buckets[n][0] and
+      len(target) < _buckets[n][1]; source and target are lists of token-ids.
+  """
+  data_set = []
+  with gfile.GFile(source_path, mode="r") as source_file:
+    source, target = source_file.readline(), source_file.readline()
+    counter = 0
+    while source and target and (not max_size or counter < max_size):
+      counter += 1
+      if counter % 100000 == 0:
+        print("  reading data line %d" % counter)
+        sys.stdout.flush()
+      source_ids = [int(x) for x in source.split()]
+      target_ids = [int(x) for x in target.split()]
+      target_ids.append(EOS_ID)
+      source, target = source_file.readline(), source_file.readline()
+      data_set.append([source_ids, target_ids])
+  return data_set
