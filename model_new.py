2b56644c83da763998f4ea1e9481b449ffbc92e6
Zhong, keli <qzk@126.com>
2017-02-22 11:35:06 +0800

    add batch size

--- model_new.py
+++ model_new.py
@@ -27,6 +27,7 @@ tf.app.flags.DEFINE_string("job_name", "worker", "One of 'ps', 'worker'")
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
 tf.app.flags.DEFINE_integer("issync", 0, "是否采用分布式的同步模式，1表示同步模式，0表示异步模式")
 tf.app.flags.DEFINE_string("gpu", None, "specify the gpu to use")
+tf.app.flags.DEFINE_integer("batch_size", 128, "batch_sizeIndex of task within the job")
 
 class Seq2SeqModel():
     """Seq2Seq model usign blocks from new `tf.contrib.seq2seq`.
@@ -306,7 +307,7 @@ def train_on_copy_task(session, model,
 
     return loss_track
 
-def create_model(checkpoint_dir, gpu="", max_batches=500000, batch_size=128):
+def create_model(checkpoint_dir, gpu="", max_batches=500000):
     ps_hosts = FLAGS.ps_hosts.split(",")
     worker_hosts = FLAGS.worker_hosts.split(",")
     print(ps_hosts)
@@ -316,6 +317,8 @@ def create_model(checkpoint_dir, gpu="", max_batches=500000, batch_size=128):
     print(worker_hosts)
     task_index = FLAGS.task_index
     job_name = FLAGS.job_name
+    batch_size = FLAGS.batch_size
+    print(batch_size)
     if(job_name == "single"):
         master = ""
     else:
