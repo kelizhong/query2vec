ccb19343dc917cd704648c08324ff7cfe3acca14
Zhong, keli <qzk@126.com>
2017-02-22 10:28:03 +0800

    add gpu flag

--- model_new.py
+++ model_new.py
@@ -26,7 +26,7 @@ tf.app.flags.DEFINE_string("worker_hosts", "0.0.0.0:2222",
 tf.app.flags.DEFINE_string("job_name", "worker", "One of 'ps', 'worker'")
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
 tf.app.flags.DEFINE_integer("issync", 0, "是否采用分布式的同步模式，1表示同步模式，0表示异步模式")
-
+tf.app.flags.DEFINE_string("gpu", None, "specify the gpu to use")
 
 class Seq2SeqModel():
     """Seq2Seq model usign blocks from new `tf.contrib.seq2seq`.
@@ -363,4 +363,5 @@ def create_model(checkpoint_dir, gpu="", job_type="single", task_id=0, max_batch
 
         sv.stop()
 if __name__ == '__main__':
-    create_model('./checkpoint')
+    gpu = FLAGS.gpu
+    create_model('./checkpoint', gpu)
